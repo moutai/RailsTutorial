f9f190148ee8f419579d04f4df75825577b505ba
moussa taifi <moutai10@gmail.com>
2013-08-04 10:30:24 -0400

    fix the Gemfile and adds the secret token generator

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,32 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = '4e8275476afb768d974c2a3f047da14e09efd10553e0d14f92ca45121964eadd6c39fc0bc066505969c3bf4287e0873703ce151749ab742fc255f255ad99af7a'
+#SampleApp::Application.config.secret_key_base = '4e8275476afb768d974c2a3f047da14e09efd10553e0d14f92ca45121964eadd6c39fc0bc066505969c3bf4287e0873703ce151749ab742fc255f255ad99af7a'
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
